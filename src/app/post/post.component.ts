75c066dccacaaa53c02b74910bc18ef85163d90e
ciovanes <iovanescuclaudiu@proton.me>
2025-02-18 18:44:33 +0100

    Update post component and making responsive

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -14,6 +14,7 @@ export class PostComponent {
     'community_image': 'https://avatars.githubusercontent.com/u/72162161?v=4',
     'community_name': 'Salinas',
     'image': 'https://gratisography.com/wp-content/uploads/2025/01/gratisography-dog-vacation-800x525.jpg',
+    // 'image': 'https://imgs.search.brave.com/dMYkbf1nBKGo6Y0QY4_FvjFiXI_LZ-LPUkpNB9lCtho/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzM4LzVj/Lzc2LzM4NWM3NjY5/ODljNmYxYzM1NGY0/YzFkZGZjMWI5YzZi/LmpwZw',
     'aura': '7000',
     'comments': '299',
     // 'posted_date': '2025-02-14',
