6823ceea626ac21b13bd726e2e27fb441fc12b05
RedondoDEV <javierRedondoDEV@gmail.com>
2025-02-18 17:21:01 +0100

    preTest importing components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FundamentalNgxCoreModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
