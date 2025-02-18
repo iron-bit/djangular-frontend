8707193b947fdb3198817ab5bcca472f87fc4e53
VulpeenGX <alejandro.prg.trb@gmail.com>
2025-02-18 20:15:57 +0100

    use dock and community components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DockIzqComponent } from './dock-izq/dock-izq.component';
+import { CommunitiesComponent } from './communities/communities.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DockIzqComponent, CommunitiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
