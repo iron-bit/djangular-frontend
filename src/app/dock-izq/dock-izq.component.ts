0dd3c8e1a7ddf7f98691adfff1bab73b1779c9bb
VulpeenGX <alejandro.prg.trb@gmail.com>
2025-02-19 21:30:22 +0100

    update styles

--- src/app/dock-izq/dock-izq.component.ts
+++ src/app/dock-izq/dock-izq.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-dock-izq',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dock-izq.component.html',
   styleUrl: './dock-izq.component.css',
 })
 export class DockIzqComponent {
+  @Output() tagSeleccionado = new EventEmitter<String>();
+
   dock_izq = {
-    
+    tags: [
+      { tag: 'all', icon: 'assets/all2.png' },
+      { tag: 'social', icon: 'assets/social.svg' },
+      { tag: 'health', icon: 'assets/health.svg' },
+      { tag: 'technology', icon: 'assets/technology.svg' },
+      { tag: 'art', icon: 'assets/art.svg' },
+      { tag: 'science', icon: 'assets/science.svg' },
+    ],
   };
+
+  seleccionarTag(tag: String) {
+    this.tagSeleccionado.emit(tag);
+  }
 }
