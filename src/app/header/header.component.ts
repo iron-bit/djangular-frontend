9db76df806d458b17866b23be18cd65bcaffcc8b
RedondoDEV <javierRedondoDEV@gmail.com>
2025-02-19 21:32:58 +0100

    responsive header done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,48 +19,53 @@ export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
 
   ngOnInit() {
-        this.items = [
-            {
-                separator: true
-            },
-            {
-                label: 'Posts',
-                items: [
-                    {
-                        label: 'New',
-                        icon: 'pi pi-plus',
-                        shortcut: 'ctrl+N'
-                    },
-                    {
-                        label: 'Search',
-                        icon: 'pi pi-search',
-                        shortcut: 'ctrl+S'
-                    }
-                ]
-            },
-            {
-                label: 'Profile',
-                items: [
-                    {
-                        label: 'Your Profile',
-                        icon: 'pi pi-user',
-                        shortcut: 'ctrl+P'
-                    },
-                    {
-                        label: 'Your Posts',
-                        icon: 'pi pi-clipboard',
-                        badge: '16', // Poner el número de posts
-                    },
-                    {
-                        label: 'Logout',
-                        icon: 'pi pi-sign-out',
-                        shortcut: 'ctrl+Q'
-                    }
-                ]
-            },
-            {
-                separator: true
-            }
-        ];
-    }
+    this.items = [{
+      separator: true
+    },
+      {
+        label: 'Posts',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+          }
+        ]
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Your Profile',
+            icon: 'pi pi-user',
+          },
+          {
+            label: 'Your Posts',
+            icon: 'pi pi-clipboard',
+            badge: '16', // Poner el número de posts
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+          }
+        ]
+      },
+      {
+        separator: true
+      }];
+  }
+
+  user = {
+    'id': '1',
+    'name': 'John Doe',
+    'nickname': 'johndoe123',
+    'email': 'john@mail.com',
+    'image': 'https://content.nationalgeographic.com.es/medio/2023/02/15/sus-incisivos-afilados-siempre-crecen_002dad44_230215174745_2000x1333.jpg',
+    'age' : 25,
+    'creation-time' : '19-02-2025',
+  }
+
 }
