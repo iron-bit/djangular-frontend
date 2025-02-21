dbf7a35a95761241e6196ccb083a20208e2eaf34
Rutherfordio78 <gutierrezpereirajaime@gmail.com>
2025-02-21 18:11:54 +0100

    Componente create-post-popup completado

---
+++ src/app/create-post-popup/create-post-popup.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-post-popup',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './create-post-popup.component.html',
+  styleUrl: './create-post-popup.component.css'
+})
+export class CreatePostPopupComponent {
+  userAvatar: string = 'https://img.freepik.com/vector-gratis/juego-joystick-tecnologia-deportiva_138676-2045.jpg';
+
+  selectedCategories = {
+    social: false,
+    science: false,
+    tecnology: false,
+    health: false,
+    art: false,
+  };
+
+  datosDelPost = {
+    title: '',
+    description: '',
+    comunity: '',
+    selectedCategories: [],
+    image: '',
+  }
+
+
+
+  toggleCategory(category: keyof typeof this.selectedCategories) {
+    this.selectedCategories[category] = !this.selectedCategories[category];
+  }
+
+  selectedFile: File | null = null;
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      // Aquí puedes agregar lógica adicional si es necesario
+    }
+  }
+
+  // Método para enviar el archivo al backend
+  uploadFile(): void {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('image', this.selectedFile, this.selectedFile.name);
+
+      // Aquí debes implementar la lógica para enviar 'formData' al backend
+      // Por ejemplo, utilizando HttpClient de Angular
+    } else {
+      console.error('No se ha seleccionado ningún archivo.');
+    }
+  }
+
+}
