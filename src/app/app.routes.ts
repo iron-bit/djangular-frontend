4314a1eb3557cf30a4a3e02b77bd93dcf2998b17
Tymur <tymurkulivar@gmail.com>
2025-02-27 02:03:01 +0100

    add user view, update header to open said route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
-import { Routes } from '@angular/router';
-import { PostsViewComponent } from './posts-view/posts-view.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import {Routes} from '@angular/router';
+import {PostsViewComponent} from './posts-view/posts-view.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AccountDataComponent} from './account-data/account-data.component';
 
 
 export const routes: Routes = [
-	{ path: '', component: PostsViewComponent },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
-	{ path: '**', component:NotFoundComponent }
+  {path: '', component: PostsViewComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: AccountDataComponent},
+  {path: '**', component: NotFoundComponent}
 ];
