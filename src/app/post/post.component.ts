4c1299446f0fd373e9d823adc04986335845bc81
VulpeenGX <alejandro.prg.trb@gmail.com>
2025-02-27 03:00:32 +0100

    Added getTimeDiff to calculate the time post and updateAura to update de aura via API and emit the updated value

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-post',
@@ -9,6 +10,7 @@ import {CommonModule} from '@angular/common';
 })
 export class PostComponent implements OnInit {
   @Input() post: any = {};
+  @Output() auraUpdated = new EventEmitter<number>(); // Para enviar el cambio de aura al post-view que es el que hace refresh de los datos
 
   private maxShowTags = 3;
   public tags: any[] = [];
@@ -22,7 +24,10 @@ export class PostComponent implements OnInit {
 
   public nsfw: boolean = false;
 
+  constructor(private apiService: ApiService) {}
+
   ngOnInit(): void {
+    console.log('POST ID de prueba', this.post.id)
     this.nsfw = this.post.is_adult === true;
     this.tags = this.post.tags ? this.post.tags.slice(0, this.maxShowTags) : [];
   }
@@ -36,4 +41,48 @@ export class PostComponent implements OnInit {
   public disableNSFW() {
     this.nsfw = false;
   }
+
+  // de aqui para abajo
+  getTimeDifference(): string | null {
+    if (!this.post.creation_date) return null;
+
+    const now = new Date();
+    const created = new Date(this.post.creation_date);
+    const diffMs = now.getTime() - created.getTime();
+    const diffHours = diffMs / (1000 * 60 * 60); 
+
+    if (diffHours < 24) {
+      const diffMinutes = Math.floor(diffMs / (1000 * 60)); 
+      if (diffHours >= 1) {
+        return `${Math.floor(diffHours)} hour${Math.floor(diffHours) !== 1 ? 's' : ''} ago`;
+      } else {
+        return `${diffMinutes} minute${diffMinutes !== 1 ? 's' : ''} ago`;
+      }
+    }
+    return null;
+  }
+
+  //Actualiza el aura con la api para que luego el otro metodo updateAura lo carge en el front 
+  updateAura(action: string): void {
+    if (action === 'sumar') {
+      this.apiService.updateAura(this.post.id, 'sumar').subscribe(
+        (response) => {
+          if (response && response.post && typeof response.post.aura === 'number') {
+            this.auraUpdated.emit(response.post.aura);  
+          } 
+        },
+      );
+    } else if (action === 'restar') {
+      this.apiService.updateAura(this.post.id, 'restar').subscribe(
+        (response) => {
+          console.log('Response:', response);
+          if (response && response.post && typeof response.post.aura === 'number') {
+            this.auraUpdated.emit(response.post.aura); 
+          }
+        }
+      );
+    }
+  }
+
+
 }
